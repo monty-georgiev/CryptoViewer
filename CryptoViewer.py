fa6ae4812a1c82365ba710c19902d51e4afee78a
monty-georgiev <monty.georgiev@gmail.com>
2018-01-11 17:49:32 +0200

    Auto refresh

--- CryptoViewer.py
+++ CryptoViewer.py
@@ -1,21 +1,56 @@
 import requests
 import json
+import time
 from tkinter import *
 from tkinter import ttk
 
 
-class CryptoViewer:
+class CryptoCurrency(object):
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
 
-    def __init__(self, master, pairs):
-        self.master = master
-        self.pairs = pairs
+    # def say_hello(self, window):
+    #     self.window = window
+    #     r = 2
+    #     total_sum = 0
+    #     eth_usd = 0
+    #     for pair in list:
+    #         pair_sum = pair[2] * pair[1]
+    #         if (pair[0] == 'ETHUSDT'):
+    #             eth_usd = float(pair[1])
+    #         ttk.Label(self.master,
+    #                   width=50,
+    #                   text="Pair: {} \n Current Price: {} \n Amount: {}  \n Total: {} \n".format(pair[0], pair[1],
+    #                                                                                              pair[2],
+    #                                                                                              pair_sum),
+    #                   font=('Courier', 14, 'bold')) \
+    #             .grid(row=r, column=1)
+    #         r += 1
+    #         total_sum += pair_sum
+    #
+    #     ttk.Label(self.master,
+    #               font=('Courier', 16, 'bold'),
+    #               width=50,
+    #               text="Total Value: {} in USD {}".format(total_sum, total_sum * eth_usd)) \
+    #         .grid(row=r, column=1)
+
+
+class CryptoViewer(object):
+
+    def __init__(self):
+        self.pairs = [('ETHUSDT', 0), ('XLMETH', 609), ('BNBETH', 3), ('XVGETH', 1717), ('ARNETH', 9), ('TNTETH', 490),
+                      ('LENDETH', 190), ('MANAETH', 150)]
+        self.master = Tk()
         self.api_url = 'https://api.binance.com/api/v1/ticker/price'
 
-        self.refresh_btn = ttk.Button(self.master,
-                              text='Refresh',
-                              command=self.refresh) \
-            .grid(row=1, column=0, columnspan=2)
+        # self.refresh_btn = ttk.Button(self.master,
+        #                               text='Refresh',
+        #                               command=self.refresh) \
+        #     .grid(row=1, column=0, columnspan=2)
+
         self.refresh()
+        self.master.mainloop()
 
     def refresh(self):
         """
@@ -25,6 +60,7 @@ class CryptoViewer:
         recent_data = self.get_current_prices()
         new_values = self.filter_price_list(recent_data, self.pairs)
         self.draw_info_pairs(new_values)
+        self.master.after(10000, self.refresh)
 
     def get_current_prices(self):
         """
@@ -42,8 +78,9 @@ class CryptoViewer:
         """
         final_list = []
         for pair in json_data:
-            if (pair['symbol'] in pairs and pair['symbol']):
-                final_list.append((pair['symbol'], pair['price']))
+            for pp in pairs:
+                if (pair['symbol'] in pp[0]):
+                    final_list.append((pair['symbol'], float(pair['price']), pp[1]))
         return final_list
 
     def draw_info_pairs(self, list):
@@ -52,20 +89,31 @@ class CryptoViewer:
         :param list: a list of tuple pairs (symbol,price)
         """
         r = 2
+        total_sum = 0
+        eth_usd = 0
         for pair in list:
+            pair_sum = pair[2] * pair[1]
+            if (pair[0] == 'ETHUSDT'):
+                eth_usd = float(pair[1])
             ttk.Label(self.master,
-                      width=30,
-                      text=pair[0] + ' = ' + pair[1],
-                      font=('Courier', 18, 'bold')) \
+                      width=50,
+                      text="Pair: {} \n Current Price: {} \n Amount: {}  \n Total: {} \n".format(pair[0], pair[1],
+                                                                                                 pair[2],
+                                                                                                 pair_sum),
+                      font=('Courier', 12, 'bold')) \
                 .grid(row=r, column=1)
             r += 1
+            total_sum += pair_sum
+
+        ttk.Label(self.master,
+                  font=('Courier', 14, 'bold'),
+                  width=50,
+                  text="Total Value: {:.6f} in USD {:.2f}".format(total_sum, total_sum * eth_usd)) \
+            .grid(row=r, column=1)
 
 
 def main():
-    root = Tk()
-    pairs = ('ETHUSDT', 'XLMETH', 'BNBETH', 'XVGETH', 'ARNETH')
-    app = CryptoViewer(root, pairs)
-    root.mainloop()
+    app = CryptoViewer()
 
 
 if __name__ == '__main__': main()
